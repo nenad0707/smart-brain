3bcf8ce9820e16c6cbb60b2d3a24302f7344b73c
Nenad Ristic <risticnenad.vr@gmail.com>
2022-08-28 00:30:41 +0200

    Update Register.js
    Changing server

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://tranquil-cove-59972.herokuapp.com/register', {
+    fetch('https://smart-brain-api-production.up.railway.app/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
