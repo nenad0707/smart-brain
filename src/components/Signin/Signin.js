2cc34ae392565898221fe570f919307814295a9d
Nenad Ristic <risticnenad.vr@gmail.com>
2022-08-28 22:31:20 +0200

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://smart-brain-api-production.up.railway.app/signin', {
+    fetch('https://smart-brain-api.onrender.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
